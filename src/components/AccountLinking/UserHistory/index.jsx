523bc47bdf39241319f7da088459f721674967a9
gohar-54 <goharaveed54@gmail.com>
2024-07-18 05:21:26 -0700

    fix: add review functionality of the website

--- src/components/AccountLinking/UserHistory/index.jsx
+++ src/components/AccountLinking/UserHistory/index.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { LucideHistory } from "lucide-react";
 import axios from "axios";
+import Modal from "react-modal";
+import ReactStars from "react-rating-stars-component";
 import { useAuth } from "@/contexts/authContext/AuthProvider";
 import { Base_Url } from "@/baseUrl";
 import Loader from "@/components/Loader";
@@ -10,11 +12,15 @@ import { useToast } from "@/components/ui/use-toast";
 const UserHistory = () => {
   const { authState } = useAuth();
   const navigate = useNavigate();
-  const {toast} = useToast()
+  const { toast } = useToast();
   const [reservations, setReservations] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [dropdownOpen, setDropdownOpen] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentReservationId, setCurrentReservationId] = useState(null);
+  const [reviewText, setReviewText] = useState("");
+  const [starRating, setStarRating] = useState(0);
 
   const redirectToUserStatistics = () => {
     navigate("/user-statistics");
@@ -62,7 +68,7 @@ const UserHistory = () => {
         (reservation) => reservation.Reservation.id !== id
       );
       toast({
-        title: " Reservation Deleted Successfully",
+        title: "Reservation Deleted Successfully",
         status: "success",
         duration: 9000,
         isClosable: true,
@@ -80,8 +86,54 @@ const UserHistory = () => {
     }
   };
 
-  const handleUpdate = (id) => {
-    navigate(`/update-reservation/${id}`);
+  const handleUpdateClick = (id) => {
+    toggleDropdown(id)
+    setCurrentReservationId(id);
+    setIsModalOpen(true);
+  };
+
+  const handleUpdateSubmit = async () => {
+
+    if(reviewText === "" || starRating === 0){
+      toast({
+        title: "Please Write your review and select the start rating",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+      return
+    }
+    try {
+      const response = await axios.patch(
+        `${Base_Url}/api/v1/reservation/add_review/${currentReservationId}/`,
+        null,
+        {
+          headers: {
+            Authorization: `Bearer ${authState?.accessToken}`,
+            accept: "application/json",
+          },
+          params: {
+            review: reviewText,
+            star_rating: starRating,
+          },
+        }
+      );
+      toast({
+        title: "Review Posted Successfully",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+      setIsModalOpen(false);
+    } catch (error) {
+      toast({
+        title: "Error occurred while Posting a review",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+      setIsModalOpen(false);
+    }
   };
 
   const toggleDropdown = (id) => {
@@ -89,7 +141,6 @@ const UserHistory = () => {
   };
 
   useEffect(() => {
-    console.log(authState , "autState")
     if (authState && authState.accessToken) {
       getUserHistory();
     }
@@ -100,7 +151,7 @@ const UserHistory = () => {
       <div>
         <Loader />
       </div>
-    );  
+    );
   }
 
   if (error) {
@@ -128,12 +179,12 @@ const UserHistory = () => {
           <table className="min-w-full bg-white">
             <thead>
               <tr className="bg-purple-600 text-white text-center">
-                <th className="text-center py-5 px-7  uppercase font-semibold text-sm">
+                <th className="text-center py-5 px-7 uppercase font-semibold text-sm">
                   Restaurant name
                 </th>
-                <th className="text-center py-5 px-7 uppercase font-semibold text-sm">
+                {/* <th className="text-center py-5 px-7 uppercase font-semibold text-sm">
                   Price
-                </th>
+                </th> */}
                 <th className="text-center py-5 px-7 uppercase font-semibold text-sm">
                   Num Diners
                 </th>
@@ -149,7 +200,7 @@ const UserHistory = () => {
                 <th className="text-center py-5 px-7 uppercase font-semibold text-sm">
                   Cuisine Type
                 </th>
-                <th className="text-center py-5 uppercase font-semibold text-sm">
+                <th className="text-center py-5 px-7 uppercase font-semibold text-sm">
                   Action
                 </th>
               </tr>
@@ -163,9 +214,9 @@ const UserHistory = () => {
                   <td className="text-center py-5 px-7">
                     {item?.Reservation?.restaurant_name}
                   </td>
-                  <td className="text-center py-5 px-7">
+                  {/* <td className="text-center py-5 px-7">
                     {item?.Reservation?.price}
-                  </td>
+                  </td> */}
                   <td className="text-center py-5 px-7">
                     {item?.Reservation?.num_diners}
                   </td>
@@ -194,10 +245,10 @@ const UserHistory = () => {
                       {dropdownOpen === item?.Reservation?.id && (
                         <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded shadow-lg z-10">
                           <button
-                            onClick={() => handleUpdate(item?.Reservation?.id)}
+                            onClick={() => handleUpdateClick(item?.Reservation?.id)}
                             className="block w-full text-center px-4 py-2 text-gray-700 hover:bg-gray-100"
                           >
-                            Update
+                            Add Review
                           </button>
                           <button
                             onClick={() => handleDelete(item?.Reservation?.id)}
@@ -215,6 +266,38 @@ const UserHistory = () => {
           </table>
         </div>
       </div>
+      <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)} style={{ content: { maxWidth:"500px", maxHeight: '300px', margin: 'auto' } }}>
+        <div className="p-6 bg-white" style={{ content: { width: '300px', height: '330px', margin: 'auto' } }}>
+          <h2 className="text-xl font-bold mb-4">Add Review</h2>
+          <textarea
+            className="w-full p-2 mb-4 border rounded"
+            placeholder="Write your review..."
+            value={reviewText}
+            onChange={(e) => setReviewText(e.target.value)}
+          />
+          <ReactStars
+            count={5}
+            onChange={(newRating) => setStarRating(newRating)}
+            size={30}
+            activeColor="#ffd700"
+            value={starRating}
+          />
+          <div className="flex justify-end mt-4">
+            <button
+              className="bg-purple-600 text-white rounded px-4 py-2 mr-2"
+              onClick={handleUpdateSubmit}
+            >
+              Submit Rating
+            </button>
+            <button
+              className="bg-gray-300 text-gray-700 rounded px-4 py-2"
+              onClick={() => setIsModalOpen(false)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
