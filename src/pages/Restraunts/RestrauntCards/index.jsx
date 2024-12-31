61f197a71ba993855069ea0270d706be718adf6f
muhammad-hassan11 <muhammad.hassan@datopian.com>
2024-12-31 16:36:38 +0500

    resturant card

--- src/pages/Restraunts/RestrauntCards/index.jsx
+++ src/pages/Restraunts/RestrauntCards/index.jsx
@@ -21,80 +21,6 @@ const RestaurantCards = memo(
 
     const user = JSON.parse(localStorage.getItem("user"));
 
-    // // Function to handle location data
-    // const getLocationData = (value) => {
-    //   console.log("Selected Location:", value);
-    // };
-
-    // // Function to handle term data
-    // const getTermData = (value) => {
-    //   console.log("Selected Term:", value);
-    //   setSearchTerm(value); // Update search term state
-    // };
-
-    // useEffect(() => {
-    //   if (
-    //     (yelpData && yelpData.length > 0 && selectedTypes.includes("yelp")) ||
-    //     (openTableData &&
-    //       openTableData.length > 0 &&
-    //       selectedTypes.includes("open_table")) ||
-    //     (resyData && resyData.length > 0 && selectedTypes.includes("resy"))
-    //   ) {
-    //     const mergedRestaurants = [];
-
-    //     if (yelpData && selectedTypes.includes("yelp")) {
-    //       mergedRestaurants.push(
-    //         ...yelpData.map((restaurant) => ({
-    //           ...restaurant,
-    //           restraunt_type: "yelp",
-    //         }))
-    //       );
-    //     }
-
-    //     if (openTableData && selectedTypes.includes("open_table")) {
-    //       mergedRestaurants.push(
-    //         ...openTableData.map((restaurant) => ({
-    //           ...restaurant,
-    //           restraunt_type: "open_table",
-    //         }))
-    //       );
-    //     }
-
-    //     if (resyData && selectedTypes.includes("resy")) {
-    //       mergedRestaurants.push(
-    //         ...resyData.map((restaurant) => ({
-    //           ...restaurant,
-    //           restraunt_type: "resy",
-    //         }))
-    //       );
-    //     }
-
-    //     // Deterministic shuffle based on restaurant name and ID
-    //     const shuffledRestaurants = mergedRestaurants.sort((a, b) => {
-    //       const keyA = (a.name + a.id).toLowerCase();
-    //       const keyB = (b.name + b.id).toLowerCase();
-    //       return keyA.localeCompare(keyB);
-    //     });
-
-    //     const matchedRestaurant = shuffledRestaurants.find((restaurant) => {
-    //       const name = restaurant.name.toLowerCase();
-    //       return name === searchTerm.toLowerCase() && searchTerm !== "";
-    //     });
-
-    //     const filteredRestaurants = shuffledRestaurants.filter((restaurant) => {
-    //       const name = restaurant.name.toLowerCase();
-    //       return !(name === searchTerm.toLowerCase() && searchTerm !== "");
-    //     });
-
-    //     if (matchedRestaurant) {
-    //       filteredRestaurants.unshift(matchedRestaurant);
-    //     }
-
-    //     setShuffledRestaurants(filteredRestaurants);
-    //   } else {
-    //     setShuffledRestaurants([]);
-    //   }
-    // }, [yelpData, openTableData, resyData, selectedTypes, searchTerm]);
     useEffect(() => {
       if (
         (yelpData && selectedTypes.includes("yelp")) ||
@@ -342,9 +268,10 @@ const RestaurantCards = memo(
                 )}`,
               }}
             >
-              <div className="bg-white w-full mb-2 p-1 md:h-[15rem] lg:h-[15rem] shadow-xl rounded-2xl flex flex-col md:flex-row lg:flex-row card text-grey-darkest">
-                <img
-                  className="w-full md:w-1/3 lg:w-1/3 h-1/3 md:h-full lg:h-full rounded-2xl object-cover"
+              <div className="bg-white w-full mb-2 p-10  shadow-xl rounded-2xl flex flex-col md:flex-row lg:flex-row card text-grey-darkest">
+              <div className="w-full md:w-1/3 lg:w-1/3 md:h-[15rem] lg:h-[20rem]">
+              <img
+                  className="h-1/3 md:h-full lg:h-full w-full rounded-2xl object-cover"
                   src={
                     data?.restraunt_type === "yelp"
                       ? data?.image_url
@@ -356,109 +283,122 @@ const RestaurantCards = memo(
                   }
                   alt={data?.name}
                 />
-                <div className="w-full md:w-1/2 lg:w-1/2 flex flex-col px-2 select-none">
-                  <div className="p-4 pb-0 flex-1">
-                    <h1 className="text-xl  font-light mb-1 text-grey-darkest">
+              </div>
+                <div className="w-full md:w-1/2 lg:w-1/2 flex flex-col px-2  select-none">
+                  <div className=" p-5 flex-1">
+                    <h1 className="text-xl  font-bold mb-1 text-grey-darkest">
                       <strong>
                         {data?.name?.length > 50
                           ? `${data?.name?.slice(0, 50)}...`
                           : data?.name}
                       </strong>
                     </h1>
-                    <br />
-                    <span className="text-grey-darkest">
-                      <h2 className="font-bold">Rating</h2>
-                      <i className="fas fa-map-marker-alt mr-1 text-grey-dark"></i>
-                      {data.restraunt_type === "yelp"
-                        ? data?.rating
-                        : data.restraunt_type === "open_table"
-                        ? data?.statistics?.reviews?.ratings?.overall?.rating
-                        : data.restraunt_type === "resy"
-                        ? data?.rating?.average
-                        : null}
-                      <span className="text-sm sm:text-base">/5</span>
-                    </span>
+                  
+                   <div  className="text-grey-darkest py-10 flex flex-col space-y-6">
+                   <div>
+                 
+                    <h4 className="font-semibold flex gap-3 items-center text-[1.25rem]"> 
+                      <img src="/assets/ratings.png" alt="ratings logo" className="h-5 w-5"/>
+                     <span>Ratings:</span>
+                     
+                   </h4>
+                
+                   <i className="fas fa-map-marker-alt mr-1 text-grey-dark"></i>
+                   {data.restraunt_type === "yelp"
+                     ? data?.rating
+                     : data.restraunt_type === "open_table"
+                     ? data?.statistics?.reviews?.ratings?.overall?.rating
+                     : data.restraunt_type === "resy"
+                     ? data?.rating?.average
+                     : null}
+                   <span className="text-sm sm:text-base">/5</span>
+                 </div>
 
-                    {data?.statistics?.recentReservationCount > 0 && (
-                      <div className="flex justify-between text-sm items-center">
-                        <div className="flex items-center">
-                          <span>
-                            <strong>
-                              Booked {data.statistics.recentReservationCount}{" "}
-                              times today
-                            </strong>
-                          </span>
-                        </div>
-                      </div>
-                    )}
-                    <div className="flex items-center mt-4">
-                      <div className="pr-2 text-base">
-                        <h2 className="font-bold">Address:</h2>
-                        <div>
-                          {data.restraunt_type === "yelp" ? (
-                            <p>{data?.location?.display_address}</p>
-                          ) : data?.restraunt_type === "open_table" ? (
-                            <div>
-                              <p>
-                                {data?.address?.line1 &&
-                                  `${data?.address?.line1} `}
-                                {data?.address?.city}
-                              </p>
-                            </div>
-                          ) : data?.restraunt_type === "resy" ? (
-                            <div>
-                              {data?.locality && `${data?.locality} `}
-                              {data?.location?.name}
-                            </div>
-                          ) : null}
-                        </div>
-                      </div>
-                    </div>
+               
+                 
+                   <div className="stext-base">
+                   <h4 className="font-semibold flex gap-3 items-center text-[1.25rem]">
+                     <img src="/assets/address.png" alt="address logo" className="h-5 w-5"  />
+                     <span>Address:</span>
+                     </h4>
+                     <div>
+                       {data.restraunt_type === "yelp" ? (
+                         <p>{data?.location?.display_address}</p>
+                       ) : data?.restraunt_type === "open_table" ? (
+                         <div>
+                           <p>
+                             {data?.address?.line1 &&
+                               `${data?.address?.line1} `}
+                             {data?.address?.city}
+                           </p>
+                         </div>
+                       ) : data?.restraunt_type === "resy" ? (
+                         <div>
+                           {data?.locality && `${data?.locality} `}
+                           {data?.location?.name}
+                         </div>
+                       ) : null}
+                     </div>
+                   </div>
+                   <div className="pr-2 text-base">
+                   <h4 className="font-semibold flex gap-3 items-center text-[1.25rem]">
+                     <img src="/assets/contact.png" alt="address logo" className="h-5 w-5"  />
+                     <span>Contact:</span>
+                     </h4>
+                     <div>
+                       {data.restraunt_type === "yelp" ? (
+                         <p>{data?.location?.display_address}</p>
+                       ) : data?.restraunt_type === "open_table" ? (
+                         <div>
+                           <p>
+                             {data?.address?.line1 &&
+                               `${data?.address?.line1} `}
+                             {data?.address?.city}
+                           </p>
+                         </div>
+                       ) : data?.restraunt_type === "resy" ? (
+                         <div>
+                           {data?.locality && `${data?.locality} `}
+                           {data?.location?.name}
+                         </div>
+                       ) : null}
+                     </div>
+                   </div>
+
+
+
+                
+                   </div>
                   </div>
                 </div>
-                <div className="hidden md:block lg:block border-l border-gray-300 h-44 my-6"></div>
+                <div className="hidden md:block lg:block  border-gray-300  my-6"></div>
                 <div className="w-full flex justify-center items-center md:w-[150px] lg:w-[150px] mx-auto md:mx-6 lg:mx-6">
-                  <div className="flex flex-col items-center">
-                    {data.restraunt_type === "yelp" ? (
-                      <img
-                        src="/assets/yelp_logo_new.png"
-                        alt="Yelp Logo"
-                        width={86}
+                  <div className="flex flex-col justify-between h-full">
+                  <img
+                  src={
+                    data.restraunt_type === "yelp"
+                      ? "/assets/yelp_logo_new.png"
+                      : data.restraunt_type === "open_table"
+                      ? "/assets/opentable.png"
+                      : data.restraunt_type === "resy"
+                      ? "/assets/resy_logo_new.png"
+                      : ""
+                  }
+                  
+                        alt={`${data.restraunt_type} logo`}
+                        width={100}
                         height={64}
                         className="mb-2"
+                        
                       />
-                    ) : data.restraunt_type === "open_table" ? (
-                      <img
-                        src="/assets/opentable.png"
-                        alt="Open Table Logo"
-                        width={106}
-                        height={84}
-                        className="mb-2"
-                      />
-                    ) : data.restraunt_type === "resy" ? (
-                      <img
-                        src="/assets/resy_logo_new.png"
-                        alt="Resy Logo"
-                        width={76}
-                        height={54}
-                        className="mb-2"
-                      />
-                    ) : null}
+                  
 
-                    <div className="text-center md:mt-4 lg:mt-4">
-                      {data.restraunt_type === "yelp"
-                        ? data?.display_phone
-                        : data.restraunt_type === "open_table"
-                        ? data?.contactInformation?.formattedPhoneNumber
-                        : data.restraunt_type === "resy"
-                        ? data?.contact?.phone_number
-                        : null}
-                    </div>
+                  <div className="flex-grow"></div>
+                    <div className="bg-grey-lighter  flex items-center justify-between transition hover:bg-grey-light cursor-pointer mt-2">
+                      <button className="rounded-full py-3 px-5 bg-purple-600 text-white ">
 
-                    <div className="bg-grey-lighter p-3 flex items-center justify-between transition hover:bg-grey-light cursor-pointer mt-2">
-                      <button className="rounded-full bg-purple-600 text-white p-2">
-                        Detail
-                      </button>
+                                Reserve a Table                     
+                          </button>
                       <i className="fas fa-chevron-right"></i>
                     </div>
                   </div>
