f37154f136a12bfac6fc0c1a5c77e6a281a1b291
Afroze Najam <afrozenajam@gmail.com>
2025-01-23 19:42:40 +0500

    fix-Reservation History

--- src/pages/Restraunts/ResturantDetailV2/ReservationsHistory.jsx
+++ src/pages/Restraunts/ResturantDetailV2/ReservationsHistory.jsx
@@ -13,8 +13,9 @@ export default function ReservationsHistory({reservations}) {
 <>
 
 <div className="my-4">
-    <div className="flex justify-between items-center"><h1 className='font-bold text-3xl my-4'>Up Coming Rservations</h1>
-    <Link className='bg-plum p-3 rounded-full text-white' to="/user-profile">
+    <div className="flex justify-between items-center">
+      <p className='font-bold text-4xl font-agrandir text-shipGrey my-4'>Upcoming Reservations</p>
+    <Link className='bg-plum p-3 px-4 rounded-full font-agrandir font-bold text-base text-white' to="/user-profile">
     Go to your Dining History
     </Link>
     </div>
@@ -115,11 +116,11 @@ export default function ReservationsHistory({reservations}) {
       </div>
     </div>
   </div>
-  )): <h4 className='text-lg text-plum font-raleWay italic p-5 '>No Up Coming Reservation</h4>}
+  )): <h4 className='text-lg text-plum font-agrandir italic p-5 '>No Up Coming Reservation</h4>}
 
 </div>
 <div className='my-12 py-10'>
-<h1 className='font-bold text-3xl my-4'>Past Reservations</h1>
+<h1 className='font-bold text-4xl font-agrandir text-shipGrey my-4'>Past Reservations</h1>
 {pastReservations.map((reservation) =>(
     <div className="bg-white w-full mb-2 p-10  shadow-xl rounded-2xl flex flex-col md:flex-row lg:flex-row card text-grey-darkest">
     <div className="w-full md:w-1/3 lg:w-1/3 md:h-[15rem] lg:h-[20rem] flex items-center rounded-lg justify-center bg-[#F5EDFC] relative overflow-hidden">
@@ -136,9 +137,9 @@ export default function ReservationsHistory({reservations}) {
 
     <div className="w-full md:w-1/2 lg:w-1/2 flex flex-col px-2  select-none">
       <div className=" p-5 flex-1">
-        <h1 className="text-3xl mb-1 font-semibold text-grey-darkest">
+        <p className="text-4xl mb-1 font-bold text-shipGrey font-agrandir">
           {reservation?.restaurant_name}
-        </h1>
+        </p>
 
         <div className="text-grey-darkest py-8 flex flex-col space-y-4">
           <div>
@@ -148,7 +149,7 @@ export default function ReservationsHistory({reservations}) {
                 alt="ratings logo"
                 className="h-4 w-4 md:h-5 md:w-5"
               />
-              <span>Reservation Date:</span>
+              <span className="font-roboto font-semibold text-xl text-shipGrey">Reservation Date: </span>
             </p>
 
             <p className="px-8">
@@ -163,7 +164,7 @@ export default function ReservationsHistory({reservations}) {
                 alt="address logo"
                 className="h-4 w-4 md:h-5 md:w-5"
               />
-              <span>Address:</span>
+              <span className="font-roboto font-semibold text-xl text-shipGrey">Address: </span>
             </p>
             <p className="px-8 text-sm md:text-base">
             {reservation?.location}
@@ -177,7 +178,7 @@ export default function ReservationsHistory({reservations}) {
                 alt="address logo"
                 className="h-4 w-4"
               />
-              <span>Number of Dinners:</span>
+              <span className="font-roboto font-semibold text-xl text-shipGrey">Number of Dinners: </span>
             </p>
             <p className="px-8">
             {reservation?.num_diners} <span className='mx-1 text-sm md:text-base'>persons</span>
@@ -207,8 +208,8 @@ export default function ReservationsHistory({reservations}) {
 
         <div className="flex-grow"></div>
         {new Date (reservation?.reservation_date) < new Date() &&  <div className="bg-grey-lighter  flex items-center justify-between transition hover:bg-grey-light cursor-pointer mt-2">
-          <button className="rounded-full p-3 bg-purple-600 text-white ">
-       <span>Reserve again</span>
+          <button className="rounded-full px-3 p-2 bg-plum text-white ">
+              <span>Reserve again</span>
           </button>
           <i className="fas fa-chevron-right"></i>
         </div>}
