b6317463e5b1199f68dec453ae281fbba9de5f3e
afrozepal <afrozenajam@gmail.com>
2025-06-20 14:13:09 +0500

    fix

--- src/pages/Landing/stats/Stats.jsx
+++ src/pages/Landing/stats/Stats.jsx
@@ -2,14 +2,14 @@ import { stats } from "../../../components/constants/constants";
 
 function Stats() {
   return (
-    <div className="p-2 px-14 sm:px-0 sm:p-16 md:p-28 bg-[#F5EDFC] relative">
+    <div className="p-2 px-14 mt-36 sm:mt-40 md:mt-0 sm:px-0 sm:p-16 md:p-28 bg-[#F5EDFC] relative">
       <img
         src="/assets/sushi.png"
         alt="Sushi illustration"
-        className="hidden xl:block absolute left-10 top-[-10rem]"
+        className="absolute w-44 top-[-11rem] left-[50%] sm:left-[55%] sm:top-[-12rem] sm:z-0 sm:w-[300px] md:left-0 md:top-[-1rem] md:w-[350px] lg:top-[-5rem] lg:w-[390px] xl:w-[440px] xl:left-10 xl:top-[-10rem]"
       />
 
-      <div className="flex flex-col md:flex-row items-end justify-between gap-6">
+      <div className="sm:z-10 md:z-0 relative flex flex-col md:ml-32 lg:ml-44 md:flex-row items-end justify-between gap-6">
         <div className="w-full md:w-auto text-center md:text-left">
         </div>
 
@@ -18,9 +18,9 @@ function Stats() {
             <div
               key={stat.heading}
               className={`flex-1 p-4 w-full md:w-auto flex flex-col items-center text-center
-              ${index !== stats.length - 1 ? "border-b-[2px] border-plum md:border-r-[3px] md:border-plum" : ""}`}
+              ${index !== stats.length - 1 ? "border-b-[2px] md:border-b-0 border-plum md:border-r-[3px] md:border-plum" : ""}`}
             >
-              <p className="text-3xl md:text-4xl text-shipGrey font-extrabold font-agrandirHeavy">
+              <p className="sm:text-2xl md:text-3xl lg:text-4xl text-shipGrey font-extrabold font-agrandirHeavy">
                 {stat.stat}
               </p>
               <p className="text-sm md:text-base text-shipGrey font-roboto">
