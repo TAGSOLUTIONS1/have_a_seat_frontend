1232b49d85ae212f34b8207be4310f3136a8dadf
afrozepal <afrozenajam@gmail.com>
2025-07-15 19:42:29 +0500

    fix

--- src/pages/Restraunts/RestrauntDetailPage/index.jsx
+++ src/pages/Restraunts/RestrauntDetailPage/index.jsx
@@ -58,12 +58,13 @@ const RestrauntDetail = () => {
     setPrevId(resy_alias);
       setKey("resy");
       setEndPoint(
-        `${Base_Url}/api/v1/resy/get_restaurant_details/?venue_id=${resy_alias}&persons=2&date=${formattedDate}`
+        `${Base_Url}/api/v1/resy/get_restaurant_details?venue_id=${resy_alias}&persons=2&date=${formattedDate}`
+        // `${Base_Url}/api/v1/resy/get_restaurant_details_v2?url_slug=omakase-ichi&location=new-york-ny`
       );
     } 
     else {
       null;
-    }
+    }setPrevId
   }, [location.search, prevId]);
 
   useEffect(() => {
